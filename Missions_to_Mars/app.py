72f97c78f93e22621ed64fceb48be952a044d400
ALCILUZ GOMEZ <galciluz@gmail.com>
2021-03-18 21:58:15 -0400

    adding scrape file and html file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -8,16 +8,15 @@ app = Flask(__name__)
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=destination_data)
+    return render_template("index.html", mars=mars_data)
 
 
 # Route that will trigger the scrape function
